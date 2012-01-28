5c4ae14967e4492f58a8d7ed2870b4c31618c8a3
Yonatan <yonatan.graber@gmail.com>
2012-01-28 19:48:00 +0200

    Refactored the seperators into consts

--- src/main/java/me/yonatan/spicyurl/Url.java
+++ src/main/java/me/yonatan/spicyurl/Url.java
@@ -35,12 +35,12 @@ public class Url {
 		Parser p = new Parser(this);
 		p.parse();
 	}
-	
-	public Url(URL url){
+
+	public Url(URL url) {
 		this(Preconditions.checkNotNull(url).toString());
 	}
-	
-	public Url(URI uri){
+
+	public Url(URI uri) {
 		this(Preconditions.checkNotNull(uri).toString());
 	}
 
@@ -56,6 +56,14 @@ public class Url {
 
 	class Parser {
 		private final static String SCHEME_SEP = "://";
+		private final static String PATH_SEP = "/";
+		private final static String LOGIN_SEP = "@";
+		private final static String USER_PASS_SEP = ":";
+		private final static String PORT_SEP = ":";
+		private final static int MIN_PORT_VALUE = 1;
+		private final static int MAX_PORT_VALUE = 65535;
+		private final static String QUERY_SEP = "?";
+		private final static String FRAGMENT_SEP = "?";
 
 		private final Url url;
 
@@ -75,7 +83,8 @@ public class Url {
 				throw new MalformedUrlException();
 
 			String[] stage1 = StringUtils
-					.splitByWholeSeparatorPreserveAllTokens(stage0[1], "/", 2);
+					.splitByWholeSeparatorPreserveAllTokens(stage0[1],
+							PATH_SEP, 2);
 			if (stage1.length == 0)
 				throw new MalformedUrlException();
 
@@ -87,10 +96,11 @@ public class Url {
 		private void parseLoginHostPort(String loginHostPort) {
 			if (StringUtils.isEmpty(loginHostPort))
 				throw new MalformedUrlException();
-			if (loginHostPort.contains("@")) {
-				parseLogin(StringUtils.substringBeforeLast(loginHostPort, "@"));
+			if (loginHostPort.contains(LOGIN_SEP)) {
+				parseLogin(StringUtils.substringBeforeLast(loginHostPort,
+						LOGIN_SEP));
 				parseHostPort(StringUtils.substringAfterLast(loginHostPort,
-						"@"));
+						LOGIN_SEP));
 			} else {
 				parseHostPort(loginHostPort);
 			}
@@ -99,17 +109,19 @@ public class Url {
 		private void parseLogin(String login) {
 			if (StringUtils.isEmpty(login))
 				return;
-			
-			if (login.contains(":")){
-				url.username=StringUtils.substringBefore(login, ":");
-				url.password=StringUtils.substringAfter(login, ":");
+
+			if (login.contains(USER_PASS_SEP)) {
+				url.username = StringUtils
+						.substringBefore(login, USER_PASS_SEP);
+				url.password = StringUtils.substringAfter(login, USER_PASS_SEP);
 			} else {
-				url.username=login;
+				url.username = login;
 			}
 		}
 
 		private void parseHostPort(String hostPort) {
-			String[] stage0 = StringUtils.splitPreserveAllTokens(hostPort, ':');
+			String[] stage0 = StringUtils.splitPreserveAllTokens(hostPort,
+					PORT_SEP);
 			if (stage0.length > 2)
 				throw new MalformedUrlException();
 			url.host = stage0[0];
@@ -129,27 +141,30 @@ public class Url {
 			} catch (NumberFormatException e) {
 				throw new MalformedUrlException();
 			}
-			if (url.port <= 0 || url.port > 65535)
+			if (url.port < MIN_PORT_VALUE || url.port > MAX_PORT_VALUE)
 				throw new MalformedUrlException();
 		}
 
 		private void parsePathQueryFregment(String pathQueryFregment) {
-			//if (StringUtils.isEmpty(pathQueryFregment)) return;
-			if (pathQueryFregment==null) return;
-			String[] stage0=StringUtils.splitPreserveAllTokens(pathQueryFregment, "#", 2);
-			if (stage0.length==2){
-				url.fragment=stage0[1];
+			// if (StringUtils.isEmpty(pathQueryFregment)) return;
+			if (pathQueryFregment == null)
+				return;
+			String[] stage0 = StringUtils.splitPreserveAllTokens(
+					pathQueryFregment, FRAGMENT_SEP, 2);
+			if (stage0.length == 2) {
+				url.fragment = stage0[1];
 			}
 			parsePathQuery(stage0[0]);
 		}
-		
-		private void parsePathQuery(String pathQuery){
-			//if (StringUtils.isEmpty(pathQuery)) return;
-			String[] stage0=StringUtils.splitPreserveAllTokens(pathQuery, "?", 2);
-			if (stage0.length==2){
-				url.query=stage0[1];
+
+		private void parsePathQuery(String pathQuery) {
+			// if (StringUtils.isEmpty(pathQuery)) return;
+			String[] stage0 = StringUtils.splitPreserveAllTokens(pathQuery,
+					QUERY_SEP, 2);
+			if (stage0.length == 2) {
+				url.query = stage0[1];
 			}
-			url.path=stage0[0];
+			url.path = stage0[0];
 		}
 	}
 
