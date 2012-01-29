2cd516a032649ebbef6bd48c369c9a783c09dee1
Yonatan <yonatan.graber@gmail.com>
2012-01-29 20:29:32 +0200

    Refactored UrlParser out of the Url class

--- src/main/java/org/spicyurl/Url.java
+++ src/main/java/org/spicyurl/Url.java
@@ -26,7 +26,6 @@ import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Getter;
 
-import org.apache.commons.lang3.CharUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Preconditions;
@@ -37,7 +36,7 @@ public class Url {
 	public Url(String url) {
 		Preconditions.checkArgument(StringUtils.isNotBlank(url));
 		raw = url;
-		Parser p = new Parser(this);
+		UrlParser p = new UrlParser(this);
 		p.parse();
 	}
 
@@ -67,19 +66,19 @@ public class Url {
 	private String query;
 	private String fragment;
 
-	@Getter(AccessLevel.NONE)
-	private Set<Errors> _validationErrors = new HashSet<Errors>();
+	@Getter(AccessLevel.PROTECTED) 
+	private Set<Errors> validationErrorsModifiable = new HashSet<Errors>();
 
 	private Set<Errors> valiationErrors = Collections
-			.unmodifiableSet(_validationErrors);
+			.unmodifiableSet(validationErrorsModifiable);
 
 	public boolean isValid() {
-		return _validationErrors.isEmpty();
+		return validationErrorsModifiable.isEmpty();
 	}
 
 	protected void setHost(String host) {
 		if (StringUtils.isEmpty(host)) {
-			_validationErrors.add(Errors.HOST_IS_MISSING);
+			validationErrorsModifiable.add(Errors.HOST_IS_MISSING);
 			return;
 		}
 		this.host = host;
@@ -87,7 +86,7 @@ public class Url {
 
 	protected void setScheme(String scheme) {
 		if (StringUtils.isEmpty(scheme)) {
-			_validationErrors.add(Errors.SCHEME_IS_MISSING);
+			validationErrorsModifiable.add(Errors.SCHEME_IS_MISSING);
 			return;
 		}
 		this.scheme = scheme;
@@ -98,8 +97,8 @@ public class Url {
 	}
 
 	protected void setPort(int port) {
-		if (port < Parser.MIN_PORT_VALUE || port > Parser.MAX_PORT_VALUE) {
-			_validationErrors.add(Errors.INVALID_PORT_VALUE);
+		if (port < UrlParser.MIN_PORT_VALUE || port > UrlParser.MAX_PORT_VALUE) {
+			validationErrorsModifiable.add(Errors.INVALID_PORT_VALUE);
 			return;
 		}
 
@@ -120,159 +119,6 @@ public class Url {
 
 	protected void setFragment(String fragment) {
 		this.fragment = fragment;
-	}
-
-	protected class Parser {
-		private final static String SCHEME_SEP = "://";
-		private final static char QUERY_SEP_CHAR = '?';
-		private final static char FRAGMENT_SEP_CHAR = '#';
-		private final static char PATH_SEP_CHAR = '/';
-		private final static String LOGIN_SEP = "@";
-		private final static String USER_PASS_SEP = ":";
-		private final static String PORT_SEP = ":";
-		private final static int MIN_PORT_VALUE = 1;
-		private final static int MAX_PORT_VALUE = 65535;
-
-		private final static String QUERY_SEP = "" + QUERY_SEP_CHAR;
-		private final static String PATH_SEP = "" + PATH_SEP_CHAR;
-		private final static String FRAGMENT_SEP = "" + FRAGMENT_SEP_CHAR;
-
-		private final Url url;
-
-		protected Parser(Url url) {
-			this.url = url;
-		}
-
-		protected void parse() {
-			String[] stage0 = StringUtils
-					.splitByWholeSeparatorPreserveAllTokens(url.getRaw(),
-							SCHEME_SEP, 2);
-
-			url.setScheme(stage0[0]);
-			if (stage0.length != 2) {
-				_validationErrors.add(Errors.HOST_IS_MISSING);
-				return;
-			}
-
-			// Check for first separator, to split host from path/query/fragment
-			int hostSeperatorIdx = StringUtils.indexOfAny(stage0[1], PATH_SEP,
-					QUERY_SEP, FRAGMENT_SEP);
-			if (hostSeperatorIdx == -1) {
-				// Just host
-				parseLoginHostPort(stage0[1]);
-			} else {
-				parseLoginHostPort(StringUtils.substring(stage0[1], 0,
-						hostSeperatorIdx));
-				parsePathQueryFregment(StringUtils.substring(stage0[1],
-						hostSeperatorIdx));
-			}
-
-		}
-
-		private void parseLoginHostPort(String loginHostPort) {
-			if (StringUtils.isEmpty(loginHostPort)) {
-				_validationErrors.add(Errors.HOST_IS_MISSING);
-				return;
-			}
-			if (loginHostPort.contains(LOGIN_SEP)) {
-				parseLogin(StringUtils.substringBeforeLast(loginHostPort,
-						LOGIN_SEP));
-				parseHostPort(StringUtils.substringAfterLast(loginHostPort,
-						LOGIN_SEP));
-			} else {
-				parseHostPort(loginHostPort);
-			}
-		}
-
-		private void parseLogin(String login) {
-			if (StringUtils.contains(login, USER_PASS_SEP)) {
-				url.setUsername(StringUtils.substringBefore(login,
-						USER_PASS_SEP));
-				url.setPassword(StringUtils
-						.substringAfter(login, USER_PASS_SEP));
-			} else {
-				url.setUsername(login);
-			}
-		}
-
-		private void parseHostPort(String hostPort) {
-			String[] stage0 = StringUtils.splitPreserveAllTokens(hostPort,
-					PORT_SEP);
-			if (stage0.length > 2) {
-				_validationErrors.add(Errors.INVALID_PORT_VALUE);
-				return;
-			}
-			url.setHost(stage0[0]);
-			if (stage0.length == 2) {
-				parsePort(stage0[1]);
-			}
-		}
-
-		private void parsePort(String port) {
-			if (!StringUtils.isNumeric(port)) {
-				_validationErrors.add(Errors.INVALID_PORT_VALUE);
-				return;
-			}
-			try {
-				url.setPort(Integer.parseInt(port));
-			} catch (NumberFormatException e) {
-				_validationErrors.add(Errors.INVALID_PORT_VALUE);
-				return;
-			}
-		}
-
-		/**
-		 * 
-		 * @param pathQueryFregment
-		 *            - first char should be the seperator (PATH '/', QUERY '?'
-		 *            or FRAGMENT '?')
-		 */
-		private void parsePathQueryFregment(String sepPathQueryFregment) {
-			char firstSeperator = CharUtils.toChar(sepPathQueryFregment);
-			String pathQueryFregment = StringUtils.substring(
-					sepPathQueryFregment, 1);
-
-			switch (firstSeperator) {
-			case PATH_SEP_CHAR:
-				int secondSeperatorIdx = StringUtils.indexOfAny(
-						pathQueryFregment, QUERY_SEP, FRAGMENT_SEP);
-				if (secondSeperatorIdx == -1) {
-					url.setPath(pathQueryFregment);
-				} else {
-					url.setPath(StringUtils.substring(pathQueryFregment, 0,
-							secondSeperatorIdx));
-					if (pathQueryFregment.charAt(secondSeperatorIdx) == FRAGMENT_SEP_CHAR) {
-						url.setFragment(StringUtils.substring(
-								pathQueryFregment, secondSeperatorIdx + 1));
-					} else {
-						parseQueryFregmant(StringUtils.substring(
-								pathQueryFregment, secondSeperatorIdx + 1));
-					}
-				}
-				break;
-			case QUERY_SEP_CHAR:
-				parseQueryFregmant(pathQueryFregment);
-				break;
-			case FRAGMENT_SEP_CHAR:
-				url.setFragment(pathQueryFregment);
-				break;
-			}
-		}
-
-		private void parseQueryFregmant(String queryFragment) {
-			if (StringUtils.isEmpty(queryFragment)){
-				url.setQuery(queryFragment);
-				return;
-			}
-
-			String[] parts = StringUtils.splitPreserveAllTokens(queryFragment,
-					FRAGMENT_SEP, 2);
-			url.setQuery(parts[0]);
-			if (parts.length == 2) {
-				url.setFragment(parts[1]);
-			}
-		}
-
 	}
 
 	public static enum Errors {
---
+++ src/main/java/org/spicyurl/UrlParser.java
@@ -0,0 +1,158 @@
+package org.spicyurl;
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.spicyurl.Url.Errors;
+
+class UrlParser {
+	/**
+	 * 
+	 */
+	private final static String SCHEME_SEP = "://";
+	private final static char QUERY_SEP_CHAR = '?';
+	private final static char FRAGMENT_SEP_CHAR = '#';
+	private final static char PATH_SEP_CHAR = '/';
+	private final static String LOGIN_SEP = "@";
+	private final static String USER_PASS_SEP = ":";
+	private final static String PORT_SEP = ":";
+	final static int MIN_PORT_VALUE = 1;
+	final static int MAX_PORT_VALUE = 65535;
+
+	private final static String QUERY_SEP = "" + QUERY_SEP_CHAR;
+	private final static String PATH_SEP = "" + PATH_SEP_CHAR;
+	private final static String FRAGMENT_SEP = "" + FRAGMENT_SEP_CHAR;
+
+	private final Url url;
+
+	protected UrlParser(Url url) {
+		this.url = url;
+	}
+
+	protected void parse() {
+		String[] stage0 = StringUtils.splitByWholeSeparatorPreserveAllTokens(
+				url.getRaw(), SCHEME_SEP, 2);
+
+		url.setScheme(stage0[0]);
+		if (stage0.length != 2) {
+			url.getValidationErrorsModifiable().add(Errors.HOST_IS_MISSING);
+			return;
+		}
+
+		// Check for first separator, to split host from path/query/fragment
+		int hostSeperatorIdx = StringUtils.indexOfAny(stage0[1], PATH_SEP,
+				QUERY_SEP, FRAGMENT_SEP);
+		if (hostSeperatorIdx == -1) {
+			// Just host
+			parseLoginHostPort(stage0[1]);
+		} else {
+			parseLoginHostPort(StringUtils.substring(stage0[1], 0,
+					hostSeperatorIdx));
+			parsePathQueryFregment(StringUtils.substring(stage0[1],
+					hostSeperatorIdx));
+		}
+
+	}
+
+	private void parseLoginHostPort(String loginHostPort) {
+		if (StringUtils.isEmpty(loginHostPort)) {
+			url.getValidationErrorsModifiable().add(Errors.HOST_IS_MISSING);
+			return;
+		}
+		if (loginHostPort.contains(LOGIN_SEP)) {
+			parseLogin(StringUtils
+					.substringBeforeLast(loginHostPort, LOGIN_SEP));
+			parseHostPort(StringUtils.substringAfterLast(loginHostPort,
+					LOGIN_SEP));
+		} else {
+			parseHostPort(loginHostPort);
+		}
+	}
+
+	private void parseLogin(String login) {
+		if (StringUtils.contains(login, USER_PASS_SEP)) {
+			url.setUsername(StringUtils.substringBefore(login, USER_PASS_SEP));
+			url.setPassword(StringUtils.substringAfter(login, USER_PASS_SEP));
+		} else {
+			url.setUsername(login);
+		}
+	}
+
+	private void parseHostPort(String hostPort) {
+		String[] stage0 = StringUtils
+				.splitPreserveAllTokens(hostPort, PORT_SEP);
+		if (stage0.length > 2) {
+			url.getValidationErrorsModifiable().add(Errors.INVALID_PORT_VALUE);
+			return;
+		}
+		url.setHost(stage0[0]);
+		if (stage0.length == 2) {
+			parsePort(stage0[1]);
+		}
+	}
+
+	private void parsePort(String port) {
+		if (!StringUtils.isNumeric(port)) {
+			url.getValidationErrorsModifiable().add(Errors.INVALID_PORT_VALUE);
+			return;
+		}
+		try {
+			url.setPort(Integer.parseInt(port));
+		} catch (NumberFormatException e) {
+			url.getValidationErrorsModifiable().add(Errors.INVALID_PORT_VALUE);
+			return;
+		}
+	}
+
+	/**
+	 * 
+	 * @param pathQueryFregment
+	 *            - first char should be the seperator (PATH '/', QUERY '?' or
+	 *            FRAGMENT '?')
+	 */
+	private void parsePathQueryFregment(String sepPathQueryFregment) {
+		char firstSeperator = CharUtils.toChar(sepPathQueryFregment);
+		String pathQueryFregment = StringUtils.substring(sepPathQueryFregment,
+				1);
+
+		switch (firstSeperator) {
+		case PATH_SEP_CHAR:
+			int secondSeperatorIdx = StringUtils.indexOfAny(pathQueryFregment,
+					QUERY_SEP, FRAGMENT_SEP);
+			if (secondSeperatorIdx == -1) {
+				url.setPath(pathQueryFregment);
+			} else {
+				url.setPath(StringUtils.substring(pathQueryFregment, 0,
+						secondSeperatorIdx));
+				if (pathQueryFregment.charAt(secondSeperatorIdx) == FRAGMENT_SEP_CHAR) {
+					url.setFragment(StringUtils.substring(pathQueryFregment,
+							secondSeperatorIdx + 1));
+				} else {
+					parseQueryFregmant(StringUtils.substring(pathQueryFregment,
+							secondSeperatorIdx + 1));
+				}
+			}
+			break;
+		case QUERY_SEP_CHAR:
+			parseQueryFregmant(pathQueryFregment);
+			break;
+		case FRAGMENT_SEP_CHAR:
+			url.setFragment(pathQueryFregment);
+			break;
+		}
+	}
+
+	private void parseQueryFregmant(String queryFragment) {
+		if (StringUtils.isEmpty(queryFragment)) {
+			url.setQuery(queryFragment);
+			return;
+		}
+
+		String[] parts = StringUtils.splitPreserveAllTokens(queryFragment,
+				FRAGMENT_SEP, 2);
+		url.setQuery(parts[0]);
+		if (parts.length == 2) {
+			url.setFragment(parts[1]);
+		}
+	}
+
+}
